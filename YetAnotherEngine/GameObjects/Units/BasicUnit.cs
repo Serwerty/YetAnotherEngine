9ac8da76cbfe5945e12ff6467fdff32006f6f347
Serwerty <oleg.dovzhenko2012@gmail.com>
2017-08-15 20:58:27 +0300

    added basic units

---
+++ YetAnotherEngine/GameObjects/Units/BasicUnit.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenTK;
+using OpenTK.Graphics.OpenGL;
+
+namespace YetAnotherEngine.GameObjects.Units
+{
+    class BasicUnit : UnitBase
+    {
+        public const int UnitWidth = 64;
+        public const int UnitHeight = 64;
+
+        public BasicUnit(Vector2 location, int textureId) : base(location, textureId)
+        {
+        }
+
+        public override void Draw(Color color)
+        {
+            GL.BindTexture(TextureTarget.Texture2D, TextureId);
+
+            GL.Begin(PrimitiveType.Quads);
+            GL.Color4(color);
+
+            GL.TexCoord2(0, 0);
+            GL.Vertex2(base.Location);
+            GL.TexCoord2(1, 0);
+            GL.Vertex2(base.Location.X + UnitWidth / 2f, base.Location.Y);
+            GL.TexCoord2(1, 1);
+            GL.Vertex2(base.Location.X + UnitWidth / 2f, base.Location.Y + UnitHeight / 2f);
+            GL.TexCoord2(0, 1);
+            GL.Vertex2(base.Location.X, base.Location.Y + UnitHeight / 2f);
+
+            GL.End();
+        }
+
+        protected override void Move(Vector2 endPoint)
+        {
+            if (Location == endPoint) ();
+        }
+    }
+}
---
+++ YetAnotherEngine/GameObjects/Units/UnitBase.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenTK;
+
+namespace YetAnotherEngine.GameObjects.Units
+{
+    public abstract class UnitBase
+    {
+        public Vector2 Location { get; set; }
+
+        protected int TextureId { get; set; }
+
+        public abstract void Draw(Color color);
+
+        protected UnitBase(Vector2 location, int textureId)
+        {
+            Location = location;
+            TextureId = textureId;
+        }
+
+        protected abstract void Move(Vector2 endPoint);
+    }
+}
